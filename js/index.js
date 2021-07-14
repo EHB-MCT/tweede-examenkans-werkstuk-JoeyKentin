7a3d7ea543f2137625f72acd6da883ed5d58f915
JoeyKentin <joey.kentin@student.ehb.be>
2021-07-14 14:21:04 +0200

    Fetch in a class gestoken

--- js/index.js
+++ js/index.js
@@ -1,23 +1,25 @@
 "use strict";
 
-async function postData(url='',data ={}) {
-    const response = await fetch(url, {
-    method:'GET',// *GET, POST, PUT, DELETE, etc.
-    });
-
-    return response.json();// parses JSON response into native JavaScript objects
-}
-
-postData('https://thecrew.cc/news/read.php')
-    .then(data => {
-         console.log(data);// JSON data parsed by `data.json()` call
-    }) 
-    .catch((error) => {
-        console.log(new Error(error));
-    });
-
 class Articles {
-    function like() {
+    Render(){
+        async function postData(url='',data ={}) {
+            const response = await fetch(url, {
+            method:'GET',// *GET, POST, PUT, DELETE, etc.
+            });
+        
+            return response.json();// parses JSON response into native JavaScript objects
+        }
+        
+        postData('https://thecrew.cc/news/read.php')
+            .then(data => {
+                 console.log(data);// JSON data parsed by `data.json()` call
+            }) 
+            .catch((error) => {
+                console.log(new Error(error));
+            });
+    }   
+    
+    like() {
         
     }
-}
+} 
