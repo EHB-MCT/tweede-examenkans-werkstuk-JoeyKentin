3230295e0726ead32fce9f6a1df5ab14dc00b951
JoeyKentin <joey.kentin@student.ehb.be>
2021-08-07 14:28:10 +0200

    Endpoint in een classe opgeslaan

--- js/index.js
+++ js/index.js
@@ -1,28 +1,58 @@
 "use strict";
 
+class Article {
+	constructor(article) {
+		this.UUID = article.UUID;
+		this.title = article.title;
+		this.content = article.content;
+		this.imageURI = article.imageURI;
+		this.linkTo = article.linkTo;
+		this.publicationDate = article.publicationDate;
+		this.likes = article.likes;
+	}
+}
+
 class Articles {
     Render(){
         async function postData(url='',data ={}) {
             const response = await fetch(url, {
-            method:'GET',// *GET, POST, PUT, DELETE, etc.
+            method:'GET',
             });
         
-            return response.json();// parses JSON response into native JavaScript objects
+            return response.json();
         }
-        
-        postData('https://thecrew.cc/news/read.php')
+
+        return postData('https://thecrew.cc/news/read.php')
             .then(data => {
-                 console.log(data);// JSON data parsed by `data.json()` call
+
+				return data.news.map(element => {
+					return new Article(element);
+				});
             }) 
             .catch((error) => {
                 console.log(new Error(error));
             });
     }   
     
-    like() {
+    like(uuid) {
+		
         let ID = {
-            "UUID": []
+            "UUID": [uuid]
         };
-
+		console.log(ID);
+		async function postData(url='',data ={}) {
+            const response = await fetch(url, {
+            method:'POST',
+			body: JSON.stringify(ID)
+            });
+			console.log(response);
+            return response;
+        }
+		
+		return postData('https://thecrew.cc/news/create.php') 
+            .catch((error) => {
+                console.log(new Error(error));
+            });
     }
-} 
+}
+
